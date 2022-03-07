e4694130d5abbd58b6546e6b4a1f76d34773c60e
hanaht <hanahtesfahun@gmail.com>
2022-03-07 17:17:16 +0300

    first commit

---
+++ ceasard.py
@@ -0,0 +1,18 @@
+
+def encrypt(text,s):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) - s-65) % 26 + 65)
+        else:
+            result += chr((ord(char) - s - 97) % 26 + 97)
+ 
+    return result
+
+xt=open('file.txt','r')
+text=xt.readline()
+s = 4
+f = open("file.txt", "w")
+f.write(encrypt(text,s))
